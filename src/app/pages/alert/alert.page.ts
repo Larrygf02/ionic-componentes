f75ceebb4862203c04972f3a88ec8997e3b34806
larrygf02 <raulgf_02@hotmail.com>
2020-02-02 23:12:10 -0500

    ion alert

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  titulo: string
+  constructor(public alertController: AlertController) { 
+    this.titulo = 'App header'
+  }
 
   ngOnInit() {
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'Esto es un mensaje de alerta',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Rellene un titulo'
+        }
+      ],
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: (blash) => {
+          console.log('Confirm cancel:');
+        }
+      },{
+        text: 'Ok',
+        handler: ({titulo}) => {
+          console.log('Boton ok');
+          if (titulo !== '') {
+            this.titulo = titulo
+          }
+        }
+      }]
+    })
+    console.log(alert);
+
+    await alert.present()
   }
 
 }
